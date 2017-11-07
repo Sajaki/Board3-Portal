aff3257faf32b30456547ebc7405ec35df8702ee
Sajaki <Sajaki@users.noreply.github.com>
2017-11-07 12:40:00 +0100

    Update run-epv.sh

--- travis/run-epv.sh
+++ travis/run-epv.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 #
-# This file is part of the Board3 Portal package.
+# This file is part of the phpBB Forum Software package.
 #
-# @copyright (c) Board3 Group <http://www.board3.de>
-# @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+# @copyright (c) phpBB Limited <https://www.phpbb.com>
+# @license GNU General Public License, version 2 (GPL-2.0)
+#
+# For full copyright and license information, please see
+# the docs/CREDITS.txt file.
 #
 set -e
 set -x
 
-DB=$1
-TRAVIS_PHP_VERSION=$2
-EXTNAME=$3
+EPV=$1
+NOTESTS=$2
 
-if [ "$TRAVIS_PHP_VERSION" == "5.5" -a "$DB" == "mysqli" ]
+if [ "$EPV" == "1" -a "$NOTESTS" == "1" ]
 then
-	phpBB/ext/$EXTNAME/vendor/bin/EPV.php run --dir="phpBB/ext/$EXTNAME/"
+	cd phpBB
+	composer remove sami/sami --update-with-dependencies --dev --no-interaction
+	composer require phpbb/epv:dev-master --dev --no-interaction --ignore-platform-reqs
+	cd ../
 fi
